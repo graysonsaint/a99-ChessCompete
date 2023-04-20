bf37c932a1b3d2fbfbbf9ed78ac6db7d1f184e6c
Grayson St. Aubin <gsa@unc.edu>
2023-04-20 14:52:37 -0400

    Initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Chessboard } from 'react-chessboard';
+import { useState } from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <Chessboard />
+    </div>
+  );
+}
